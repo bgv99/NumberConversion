f2c8209d32b39054c2c503d2998ecc23cad59812
bgv99 <197402987+bgv99@users.noreply.github.com>
2025-02-09 02:21:45 +0300

    Binary Conversion Feature

--- number_conversion.c
+++ number_conversion.c
@@ -1,5 +1,36 @@
 #include <stdio.h>
 
+void binaryConversion(int n)
+{
+    int binary_array[1024];
+    int byte_separate = 0;
+    int i;
+
+    printf("%d: ", n);
+
+    for(i = 0; n > 0; i++)
+    {
+        binary_array[i] = n % 2;
+        n /= 2;
+    }
+
+    for(i = i - 1; i >= 0; i--)
+    {
+        if(byte_separate == 8)
+        {
+            printf(" ");
+
+            byte_separate = 0;
+        }
+
+        printf("%d", binary_array[i]);
+        
+        byte_separate++;
+    }
+
+    printf("\n");
+}
+
 int main()
 {
     int n;
@@ -8,7 +39,7 @@ int main()
     printf("Enter n: ");
     scanf("%d", &n);
     printf("=== Convert %d to What? ===\n", n);
-    printf("1. Hexadecimal\n2. Octal\n");
+    printf("1. Hexadecimal\n2. Octal\n3. Binary\n");
     printf("> ");
     scanf("%d", &to);
 
@@ -21,6 +52,11 @@ int main()
         
         case 2:
             printf("%d: 0%o\n", n, n);
+
+            break;
+
+        case 3:
+            binaryConversion(n);
 
             break;
         
